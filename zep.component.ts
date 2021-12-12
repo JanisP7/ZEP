0c002a3d277733a1bc5a4bf2b195d1f5f6707c37
JanisP7 <91614395+JanisP7@users.noreply.github.com>
2021-12-12 22:54:57 +0100

    Add files via upload

---
+++ zep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zep',
+  templateUrl: './zep.component.html',
+  styleUrls: ['./zep.component.css']
+})
+export class ZepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
